crdroidandroid/android_packages_apps_Updater

Updater: Change package name to avoid conflict
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

push-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-updates_dir=/data/lineageos_updates
+updates_dir=/data/crdroid_updates
 
 if [ ! -f "$1" ]; then
    echo "Usage: $0 ZIP [UNVERIFIED]"
    echo "Push ZIP to $updates_dir and add it to Updater"
    echo
-   echo "The name of ZIP is assumed to have lineage-VERSION-DATE-TYPE-* as format"
+   echo "The name of ZIP is assumed to have crDroidAndroid-VERSION-DATE-TYPE-* as format"
    echo "If UNVERIFIED is set, the app will verify the update"
    exit
 fi
@@ -52,8 +52,8 @@ adb shell chgrp cache "$zip_path_device"
 adb shell chmod 664 "$zip_path_device"
 
 # Kill the app before updating the database
-adb shell "killall org.lineageos.updater 2>/dev/null"
-adb shell "sqlite3 /data/data/org.lineageos.updater/databases/updates.db" \
+adb shell "killall com.crdroid.updater 2>/dev/null"
+adb shell "sqlite3 /data/data/com.crdroid.updater/databases/updates.db" \
     "\"INSERT INTO updates (status, path, download_id, timestamp, type, version, size)" \
     "  VALUES ($status, '$zip_path_device', '$id', $timestamp, '$type', '$version', $size)\""
 

obeny/evox_packages_apps_Updater

Change-Id: Icd2b88379e656fc716d58666405bbc1f9f05d4b6
Signed-off-by: AnierinB <anierin@evolution-x.org>

push-update.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-updates_dir=/data/lineageos_updates
+updates_dir=/data/evolution_updates
 
 if [ ! -f "$1" ]; then
    echo "Usage: $0 ZIP [UNVERIFIED]"
    echo "Push ZIP to $updates_dir and add it to Updater"
    echo
-   echo "The name of ZIP is assumed to have lineage-VERSION-DATE-TYPE-* as format"
+   echo "The name of ZIP is assumed to have EevolutionX-VERSION-DATE-TYPE-* as format"
    echo "If UNVERIFIED is set, the app will verify the update"
    exit
 fi
@@ -33,7 +33,7 @@ else
     status=2
 fi
 
-# Assume lineage-VERSION-DATE-TYPE-*.zip
+# Assume EvolutionX-VERSION-DATE-TYPE-*.zip
 zip_name=`basename "$zip_path"`
 id=`echo "$zip_name" | sha1sum | cut -d' ' -f1`
 version=`echo "$zip_name" | cut -d'-' -f2`
@@ -52,8 +52,8 @@ adb shell chgrp cache "$zip_path_device"
 adb shell chmod 664 "$zip_path_device"
 
 # Kill the app before updating the database
-adb shell "killall org.lineageos.updater 2>/dev/null"
-adb shell "sqlite3 /data/data/org.lineageos.updater/databases/updates.db" \
+adb shell "killall org.evolution.updater 2>/dev/null"
+adb shell "sqlite3 /data/data/org.evolution.updater/databases/updates.db" \
     "\"INSERT INTO updates (status, path, download_id, timestamp, type, version, size)" \
     "  VALUES ($status, '$zip_path_device', '$id', $timestamp, '$type', '$version', $size)\""
 